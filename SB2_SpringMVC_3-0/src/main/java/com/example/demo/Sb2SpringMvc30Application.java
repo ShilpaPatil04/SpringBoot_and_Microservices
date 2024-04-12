5c59bc3f4728a2d44e5171d11d8e2b9c3bd9b704
Shilpa Patil <shilpabpatil04@gmail.com>
2024-04-12 13:12:51 +0530

    Spring MVC with JSP file

---
+++ SB2_SpringMVC_3-0/src/main/java/com/example/demo/Sb2SpringMvc30Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb2SpringMvc30Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sb2SpringMvc30Application.class, args);
+	}
+
+}
